b96dc4e23408df326d0c0c1b972ad09fd09c3655
nathandb7 <nathandb7@gmail.com>
2024-02-27 23:06:23 -0300

    mongodb env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const http = require('http');
 const path = require('path');
 
@@ -11,7 +14,10 @@ const server = http.createServer(app);
 const io = new socketio.Server(server);
 
 // db connection
-mongoose.connect('mongodb://localhost/chat-database')
+const password = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://admin:${password}@chat-database.i36q0ix.mongodb.net/?retryWrites=true&w=majority&appName=chat-database`;
+
+mongoose.connect(uri)
   .then(db => console.log('db is connected'))
   .catch(err => console.log(err));
 
